90bac7aaadfda36021c8ccbba5d121f3663f4e6b
tepeszCodes <tomek.szymanderski@gmail.com>
2021-09-13 09:41:19 +0200

    (bug fix) fixed turn system

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import GameBoard from './view/GameBoard';
+import Score from './view/Score';
 
 export default function App() {
   const [gameState, setGameState] = useState(new Array(9).fill(null));
-  const [curPlayer, setCurPlayer] = useState('X');
+  const [curPlayer, setCurPlayer] = useState('player1');
+  const [playersData, setPlayersData] = useState({
+    player1: {
+      sign: 'X',
+      score: 0,
+      startedThisGame: true,
+    },
+    player2: {
+      sign: 'O',
+      score: 0,
+      startedThisGame: false,
+    }
+  })
   useEffect(() => {
-    if (checkWinner('X')) { console.log('Congrats! X wins!'); resetGame(); }
-    if (checkWinner('O')) { console.log('Congrats! O wins!'); resetGame(); }
-    if (!gameState.includes(null)) { console.log('That\'s a tie!'); resetGame(); }
+    if (checkWinner(playersData.player1.sign)) {
+      console.log('Congrats! Player 1 wins!');
+      setNewGame('player1', 'player2');
+    }
+    else if (checkWinner(playersData.player2.sign)) {
+      console.log('Congrats! Player 2 wins!');
+      setNewGame('player2', 'player1');
+    }
+    else if (!gameState.includes(null)) {
+      console.log('That\'s a tie!');
+      const startingPlayer = playersData.player1.startedThisGame ? 'player2' : 'player1';
+      setNewGame(null, startingPlayer);
+    }
+    else if (gameState.join('').length > 0) {
+      curPlayer === 'player1' ?
+        setCurPlayer('player2') :
+        setCurPlayer('player1');
+    }
   }, [gameState]);
   function checkWinner(sign) {
     if (
@@ -23,22 +51,42 @@ export default function App() {
     ) { return true; }
     else { return false; }
   }
-  function resetGame() {
+  function setNewGame(winningPlayer, startingPlayer) {
+    const newPlayersData = {
+      player1: {
+        sign: playersData.player1.sign,
+        score: winningPlayer === 'player1' ? playersData.player1.score + 1 : playersData.player1.score,
+        startedThisGame: startingPlayer === 'player1',
+      },
+      player2: {
+        sign: playersData.player2.sign,
+        score: winningPlayer === 'player2' ? playersData.player2.score + 1 : playersData.player2.score,
+        startedThisGame: startingPlayer === 'player2',
+      }
+    };
+    // console.log(playersData.player1.sign);
+    // console.log(playersData.player2.sign);
+    setPlayersData(newPlayersData);
+    setCurPlayer(startingPlayer);
     setGameState(new Array(9).fill(null));
   }
   function addSign(index) {
+    const curSign = curPlayer === 'player1' ? playersData.player1.sign : playersData.player2.sign;
     if (gameState[index] !== null) {
-      console.log(`You cannot place ${curPlayer} here!`);
+      console.log(`You cannot place ${curSign} here!`);
       return;
     }
     const newGameState = [...gameState];
-    newGameState[index] = curPlayer;
+    newGameState[index] = curSign;
+    // console.log(newGameState);
+    // console.log(newGameState[index]);
     setGameState(newGameState);
-    curPlayer === 'X' ? setCurPlayer('O') : setCurPlayer('X');
   }
   return (
     <View style={styles.container}>
+      <Text style={{color: 'white'}}>{playersData.player1.startedThisGame ? 'Player 1 starts!': 'Player 2 starts!'}</Text>
       <GameBoard gameState={gameState} pressHandler={addSign}/>
+      <Score player1Score={playersData.player1.score} player2Score={playersData.player2.score}/>
     </View>
   );
 }
@@ -48,7 +96,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     color: 'white'
   },
 });
